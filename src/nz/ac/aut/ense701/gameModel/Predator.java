de9e900fcc048bfeb406b5d767de812187cc6a2e
rajcharma <hbx6167@aut.ac.nz>
2017-06-04 16:40:55 +1200

    Update Predator.java

--- src/nz/ac/aut/ense701/gameModel/Predator.java
+++ src/nz/ac/aut/ense701/gameModel/Predator.java
@@ -10,6 +10,18 @@ package nz.ac.aut.ense701.gameModel;
 public class Predator extends Fauna
 {
 
+    private static final String[] facts = {
+                                      "The biggest threat to kiwi chicks is stoats, and to adult kiwi, dogs",
+                                      "Stoats are responsible for approximately half of kiwi chick deaths on the mainland",
+                                      "An average of 27 kiwi are killed by predators EVERY WEEK. At this rate, kiwi may disappear from the mainland in our lifetime",
+                                      "DOC uses traps and Biodegradable 1080 poison drops to catch or kill the predators",
+                                      "Rats have a major impact on New Zealand’s wildlife because they eat native animals and their eggs",
+                                      "The biggest threat to kiwi chicks is stoats, and to adult kiwi, dogs"
+                                     };
+    
+    private final int id;
+    private static int nextId = 0;
+    
     /**
      * Constructor for objects of class Predator
      * @param pos the position of the predator object
@@ -19,10 +31,14 @@ public class Predator extends Fauna
     public Predator(Position pos, String name, String description) 
     {
         super(pos, name, description);
+        id = nextId;
+        nextId++;
     } 
  
     
-
+    public String getFact(){
+        return facts[id];
+    }
 
     @Override
     public String getStringRepresentation() 
